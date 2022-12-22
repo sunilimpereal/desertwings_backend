705ba5f1ae66907c1b330f80495f82f4c3bf22f8
sunil <sunilhari496@gmail.com>
2022-12-22 10:21:20 +0530

    payment screen

--- booking/views.py
+++ booking/views.py
@@ -8,7 +8,10 @@ from rest_framework.permissions import IsAuthenticated
 from account import serializers
 from booking.models import HolidayBooking
 from booking.serializers import BookHolidayBookingSerializer, HolidayBookingSerializer, PersonSerializer, RoomSerializer
+import stripe
 # Create your views here.
+stripe.api_key = "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"
+
 
 
 # to get holiday details for home screen
@@ -38,6 +41,49 @@ class PostHolidayBookingView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class CreatePaymentSessionView(APIView):
+    def post(self, request, format=None):
+        currency = request.data["currency"]
+        product_name = request.data["product_name"]
+        unit_amount = request.data["unit_amount"]
+        quantity = request.data["quantity"]
+        
+        YOUR_DOMAIN = 'http://localhost:4242'
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                line_items=[{
+                        'price_data': {
+                            'currency': currency,
+                            'product_data': {
+                                'name':product_name ,
+                            },
+                        'unit_amount': unit_amount,
+                        },
+                        'quantity': quantity,
+                        }
+            ],
+                mode='payment',
+                payment_method_types=["card"],
+                success_url=YOUR_DOMAIN + '/success.html',
+                cancel_url=YOUR_DOMAIN + '/cancel.html',
+            )
+            response = {'session_id':checkout_session.id,'payment_intent':checkout_session.payment_intent,'session_url':checkout_session.url }
+            return Response(response, status=status.HTTP_200_OK)
+        except Exception as e:
+            response = {'session_error':str(e) }
+            return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR )
+
+    # return redirect(checkout_session.url, code=303)
+        
+class PaymentSessionSucessView(APIView):
+    def get(self,request, bookingid,sessionid, format=None):
+        holidayBookingObject = HolidayBooking.objects.filter(id=bookingid)
+        holidayBookingObject.payment_id = sessionid
+        holidayBookingObject.save()
+        response = {'session_id':sessionid,'booking_id':bookingid }
+        return Response(response, status=status.HTTP_200_OK)
+        
 
 class RoomView(APIView):
     def post(self, request, format=None):
